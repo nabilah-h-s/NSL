3749df4e32ed7cad0e365772bf5baf9a12a6bc95
haliban25 <haliban256@gmail.com>
2022-02-16 09:10:26 +0600

    Third Party Library for Adapter Pattern Example

---
+++ Design Patterns/thirdPartyFilter/CaramelFilter.java
@@ -0,0 +1,14 @@
+package thirdPartyFilter;
+import adapterPattern.Image;
+
+public class CaramelFilter {
+	
+	public void init() {
+		
+	}
+	
+	public void render(Image image) {
+		System.out.println("Applying Caramel Filter");
+	}
+
+}
